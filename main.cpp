f088917097e7c0cde2846f50f335c3e8762eb43b
Guido Poletti <guidomaria.poletti@mail.polimi.it>
2024-05-01 13:06:07 +0000

    Modified makefile accordingly

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
-#include "Matrix.hpp"
+#include "MatrixCol.hpp"
 
 using namespace algebra;
 
 int main(){
-    Matrix<int,Row> mat(3,3);
+    Matrix<int,Col> mat(3,3);
 
-    mat(2,2) = 1;
-    mat(1,2) = 1;
-    mat(2,1) = 1;
+    mat(0,0) = 1;
+    mat(1,1) = 2;
+    mat(2,2) = 3;
+
+    std::vector<int> a{1,2,3};
 
     std::cout << mat << std::endl;
 
     mat.compress();
 
-    std::cout << mat << std::endl;
+    auto res = mat * a;
+
+    for(auto i : res){
+        std::cout << i << std::endl;
+    }
 
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 release: main.cpp Matrix.hpp
-	g++ -o main main.cpp Matrix.hpp -std=c++20 
+	g++ -o main main.cpp MatrixRow.hpp MatrixCol.hpp -std=c++20 
 
 debug: main.cpp Matrix.hpp
-	g++ -o main main.cpp Matrix.hpp -g -std=c++20 -DDEBUG
+	g++ -o main main.cpp MatrixRow.hpp MatrixCol.hpp -g -std=c++20 -DDEBUG
