fb343b187197b807740130b3470f7f72533cffc3
Guido Poletti <guidomaria.poletti@mail.polimi.it>
2024-05-05 10:39:30 +0000

    Finished tests

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main(int argc, const char* argv[]){
         rhs[i] = i;
     }
 
+    std::cout << "Testing row-major matrix" << std::endl;
+
     //perform matrix-vector product with the non-compressed matrix
 
     std::cout << "Performing matrix-vector product with non-compressed matrix" << std::endl;
@@ -30,6 +32,34 @@ int main(int argc, const char* argv[]){
     std::vector<double> out = mat*rhs;
 
     auto t_end = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time is " << (t_end-t_start).count()*1E-9 << " seconds\n";
+
+    mat.compress();
+
+    //perform matrix-vector product with the compressed matrix
+
+    std::cout << "Performing matrix-vector product with compressed matrix" << std::endl;
+
+    t_start = std::chrono::high_resolution_clock::now();
+
+    out = mat*rhs;
+
+    t_end = std::chrono::high_resolution_clock::now();
+    std::cout << "Elapsed time is " << (t_end-t_start).count()*1E-9 << " seconds\n";
+
+    std::cout << "Testing column-major matrix" << std::endl;
+
+    Matrix<double,Col> mat2;
+
+    mat2.read(argv[1]);
+
+    std::cout << "Performing matrix-vector product with non-compressed matrix" << std::endl;
+
+    t_start = std::chrono::high_resolution_clock::now();
+
+    std::vector<double> out2 = mat*rhs;
+
+    t_end = std::chrono::high_resolution_clock::now();
     std::cout << "Elapsed time is " << (t_end-t_start).count()*1E-9 << " seconds\n";
 
     mat.compress();
